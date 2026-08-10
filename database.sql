3cca7a5e568747ff954021cc626f4a66351d736d
bias53745-blip <bias53745@gmail.com>
2026-08-10 15:15:20 -0300

    Delete database.sql

--- database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS services (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    category VARCHAR(50) NOT NULL,
-    duration_minutes INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
-);
